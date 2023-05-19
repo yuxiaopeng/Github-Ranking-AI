c67260712512ecc8f97bef8367e4b23941d134c8
yuxiaopeng <uxiaopeng@gmail.com>
2023-05-20 01:25:36 +0800

    Update common.py

--- source/common.py
+++ source/common.py
@@ -40,7 +40,7 @@ def get_api_repos(API_URL):
     """
     get repos of api, return repos list
     """
-    access_token = get_access_token()
+    access_token = "${{TOKEN}}"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
