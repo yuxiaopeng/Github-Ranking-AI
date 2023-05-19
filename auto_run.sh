578e30adde0249cf177fbd31b754e545b06b0734
yuxiaopeng <uxiaopeng@gmail.com>
2023-05-20 02:27:22 +0800

    Update auto_run.sh

--- auto_run.sh
+++ auto_run.sh
@@ -7,7 +7,7 @@
 echo -e "\n----------Run Time:----------"
 date
 # git pull
-python3 source/process.py ${{ github.token }} 
+python3 source/process.py $0
 # git add .
 # today=`date +"%Y-%m-%d"`
 # git commit -m "auto update $today"
