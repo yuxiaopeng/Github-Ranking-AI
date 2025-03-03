3785006fd205ca8d646069e9132fcff43b196fcf
yuxiaopeng <uxiaopeng@gmail.com>
2025-03-03 10:00:46 +0800

    Update common.py

--- source/common.py
+++ source/common.py
@@ -2,18 +2,13 @@
 import json
 import requests
 import time
-import sys
 
-# git_token = ""
-
-def __init__(self, param):
-    self.git_token: object = param
 
 def get_access_token():
-    # with open('../access_token.txt', 'r') as f:
-   #     access_token = f.read().strip()
-# access_token = git_token
-    return git_token
+    with open('../access_token.txt', 'r') as f:
+        access_token = f.read().strip()
+    return access_token
+
 
 def write_text(file_name, method, text):
     """
